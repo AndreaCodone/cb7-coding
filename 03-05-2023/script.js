809aecb277ae6f214904de50cd9efe2a7d79bfd5
AndreaCodone <105943523+AndreaCodone@users.noreply.github.com>
2023-05-03 17:38:37 +0200

    Esercitazione 3 Maggio 2023

---
+++ 03-05-2023/script.js
@@ -0,0 +1,55 @@
+// Esercizio 1 
+
+//alert("EVEN OR ODD");
+
+//let number = parseInt(prompt("Inserisci un numero e ti dirò se è pari o dispari"))
+
+//if (number%2 == 0) {
+//console.log("numero pari")
+//} 
+//else {
+//console.log("numero dispari")
+//}
+
+
+
+//**********************************************
+//Esercizio 2
+
+//alert("Even or Odd");
+
+//let number = parseInt(prompt("Inserisci un numero e ti dirò se è pari o dispari"))
+
+//let evenorodd = number%2 == 0 ? "numero pari" : "numero dispari";   //operatore ternario
+
+//console.log(evenorodd)
+
+
+
+//**********************************************
+// Avanzato
+
+//alert("CALCULATOR");
+
+//let num1 = parseFloat(+(prompt("Inserisci il primo numero:")));
+//let num2 = parseFloat(+(prompt("Inserisci il secondo numero:")));
+//let operator = prompt("Inserisci un operatore (+, -, *, /):");
+
+//function calc (operator, num1, num2) {
+    //switch (operator) {
+      //case "+":
+        //return num1 + num2;
+      //case "-":
+        //return num1 - num2;
+      //case "*":
+        //return num1 * num2;
+      //case "/":
+        //return num1 / num2;
+      //default:
+        //return "NaN";
+    //}
+  //}
+  
+  //let risultato = calc(operator, num1, num2);
+  //console.log(risultato)
+  
